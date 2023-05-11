4c7d3ea4cc37e469a0f27b7932d9ad55286be25a
malustedile <malu.stedile@gmail.com>
2023-05-10 21:20:43 -0300

    mexendo na ula

--- ula.vhd
+++ ula.vhd
@@ -4,14 +4,14 @@ use ieee.numeric_std.all;
 
 entity ula is
     port( 
-        x,y : in unsigned(15 downto 0);
-        op : in unsigned(3 downto 0);
+        x,y : in unsigned(31 downto 0);
+        op : in unsigned(5 downto 0);
         saida : out unsigned(15 downto 0)
     );
    end entity;
    architecture a_ula of ula is
    begin
-    saida <=    x+y when op = "0001" else
+    saida <=     when op = "100011" else  -- 
                 x-y when op = "0010" else
                 "0000000000000001" when op = "0100" and x>=y else
                 "0000000000000001" when op = "1000" and x<y else
